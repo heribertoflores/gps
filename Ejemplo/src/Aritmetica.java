82d55611bd6c1d9d9125142a17c98ca605d17ebc
Argus <Argus@LAPTOP-7H299I9S>
2019-04-30 08:51:30 -0500

    Se agregó método para sumar dos numeros enteros

--- Ejemplo/src/Aritmetica.java
+++ Ejemplo/src/Aritmetica.java
@@ -3,7 +3,11 @@ public class Aritmetica {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public static int suma(int a, int b) {
+		return a + b;
 	}
 
 }
